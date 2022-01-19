ec7e07d6f4ef9b355d3c4f88e5fbbe084ea6f8f4
jehdaileg <jehdailegrand12@gmail.com>
2022-01-19 04:02:19 +0200

    Finish Laravel with Vite Configuration, Inertia Server and Client side configurations, Vue configuration, tailwindcss installation and configuration

---
+++ resources/scripts/Pages/Test.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <p class="text-4xl text-blue-500">Test Done</p>
+
+    </div>
+</template>
+<script setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+	purge: [
+		'./resources/**/*.blade.php',
+		'./resources/**/*.{js,ts,vue}',
+	],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
